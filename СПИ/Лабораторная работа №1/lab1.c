ad1c16490645af42c8b926e53cb81e5a4cf4aa73
Tarvan <Ivan_tishchenko@outlook.com>
2018-02-15 11:21:56 +0700

    СПИ. Лабораторная работа №1. Fix

--- СПИ/Лабораторная работа №1/lab1.c
+++ СПИ/Лабораторная работа №1/lab1.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 
 char readBufSimbol[2];
@@ -16,18 +17,17 @@ void* thread1(void *args){
   char *check;
   FILE *in1_file = fopen("inFile1.txt", "r");
 	while(1){
-    if (finish2 == 13) count = 1;
 		if (count == 1){
       check = fgets(readBufSimbol,2,in1_file);
       strcat(endString1,readBufSimbol);
-      count = rand() % 1 + 1;
       memset(readBufSimbol,0, sizeof(readBufSimbol));
+      count = 2;
     }
     if (check == NULL){
       fclose(in1_file);
       finish1 = 1;
+      puts("Первый поток посимвольного чтения из файла завершился.");
       pthread_exit(0);
-      break;
     }
   }
   fclose(in1_file);
@@ -37,18 +37,17 @@ void* thread2(void *args){
   char *check;
 	FILE *in2_file = fopen("inFile2.txt", "r");
 	while(1){
-    if (finish1 == 13) count = 2;
     if (count == 2){
       check = fgets(readBufSimbol,2,in2_file);
       strcat(endString2,readBufSimbol);
-      count = rand() % 1 + 1;
       memset(readBufSimbol,0, sizeof(readBufSimbol));
+      count = 1;
     }
     if (check == NULL){
       fclose(in2_file);
       finish2 = 1;
+      puts("Второй поток посимвольного чтения из файла завершился.");
       pthread_exit(0);
-      break;
     }
   }
 	fclose(in2_file);
@@ -62,16 +61,9 @@ int main(int argc, char const *argv[]) {
   pthread_create(&newThread1,NULL,thread1,NULL);
   pthread_create(&newThread2,NULL,thread2,NULL);
   while(1){
-    if (finish1 == 1){
-      puts("Первый поток посимвольного чтения из файла завершился.");
-      finish1 = 13;
-    }
-    if (finish2 == 1){
-      puts("Второй поток посимвольного чтения из файла завершился.");
-      finish2 = 13;
-    }
-	  if ((finish1 == 13) && (finish2 == 13)) break;
+	  if ((finish1 == 1) && (finish2 == 1)) break;
   }
+  sleep(1);
   printf("Символы из 1 файла: %s",endString1);
   printf("Символы из 2 файла: %s",endString2);
   return 0;
